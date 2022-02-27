4370a5cd66db5a1c936512ab566cba8741009870
stazed <stazed@WildMapson.com>
2022-02-27 09:18:32 -0600

    Fix HMS display on some systems.

--- src/mainwnd.cpp
+++ src/mainwnd.cpp
@@ -2626,7 +2626,7 @@ mainwnd::tick_to_timestring (long a_tick)
     microseconds -= (hours * 60 * 60 + minutes * 60 + seconds) * 1000000UL;
 
     char tmp[32];
-    snprintf(tmp, sizeof tmp, "%03d:%d:%02d   ", hours, minutes, seconds);
+    snprintf(tmp, sizeof tmp, "%03d:%d:%02d", hours, minutes, seconds);
     return std::string(tmp);
 }
 
