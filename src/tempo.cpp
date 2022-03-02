03dc6ca453f8c5e4a3c69e744f9230e7a9117d1c
Stazed <stazed@manjo-mapson.com>
2022-03-02 17:28:12 -0600

    Cleanup.

--- src/tempo.cpp
+++ src/tempo.cpp
@@ -141,20 +141,20 @@ tempo::draw_background()
     cr->set_operator(Cairo::OPERATOR_OVER);
 
     /* clear background */
-    cr->set_source_rgb( 1.0, 1.0, 1.0);            // white FIXME
+    cr->set_source_rgb( 1.0, 1.0, 1.0);            // white
     cr->set_line_width( 1);
     cr->rectangle( 0, 0, m_window_x, m_window_y);
     cr->stroke_preserve();
     cr->fill();
 
-    cr->set_source_rgb( 0.0, 0.0, 0.0);            // black  FIXME
+    cr->set_source_rgb( 0.0, 0.0, 0.0);            // black 
     cr->set_line_width( 2.0);
     cr->move_to( 0, m_window_y - 1);
     cr->line_to( m_window_x, m_window_y - 1);
     cr->stroke();
 
     /* draw vertical lines */
-    cr->set_source_rgb( 0.6, 0.6, 0.6);            // Grey  FIXME
+    cr->set_source_rgb( 0.6, 0.6, 0.6);            // Grey 
     cr->set_line_width( 1);
 
     long tick_offset = (m_4bar_offset * 16 * c_ppqn);
@@ -224,7 +224,7 @@ tempo::draw_background()
             }
 
             // set background for tempo labels to black
-            cr->set_source_rgb( 0.0, 0.0, 0.0);    // Black FIXME
+            cr->set_source_rgb( 0.0, 0.0, 0.0);    // Black
 
             auto t = create_pango_layout(str);
             t->set_font_description(font);
@@ -236,7 +236,7 @@ tempo::draw_background()
             cr->fill();
 
             // print the BPM or [Stop] label in white
-            cr->set_source_rgb( 1.0, 1.0, 1.0);    // White FIXME
+            cr->set_source_rgb( 1.0, 1.0, 1.0);    // White
             cr->move_to( tempo_marker + 5, (m_window_y * .5) - (text_height * .5) - 3 );
 
             t->show_in_cairo_context(cr);
@@ -268,7 +268,7 @@ tempo::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
         draw_background();
     }
 
-    cr->set_source_rgb(1.0, 1.0, 1.0);  // White FIXME
+    cr->set_source_rgb(1.0, 1.0, 1.0);  // White
     cr->rectangle (0.0, 0.0, width, height);
     cr->stroke_preserve();
     cr->fill();
